4b00aa5e5585ffb96087bd95e5b9bbcb8d3214d9
MKasper09 <57452322+MKasper09@users.noreply.github.com>
2021-01-21 17:19:30 +0100

    Add files via upload

---
+++ ProjektObiektoweGUI/ProjektObiektoweGUI/SpisTransakcji.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjektObiektoweGUI
+{
+    class SpisTransakcji// : Transakcjia
+    {
+        static int liczbaTrans;
+        static List<Transakcja> spisTrans = new List<Transakcja>();
+        static List<string> listaTrans = new List<string>();
+
+        public static int LiczbaTrans { get => liczbaTrans; set => liczbaTrans = value; }
+        public static List<Transakcja> SpisTrans { get => spisTrans; set => spisTrans = value; }
+        public static List<string> ListaTrans { get => listaTrans; set => listaTrans = value; }
+
+        static public void DodajTransakcję(Transakcja transakcjia)
+        {
+            SpisTrans.Add(transakcjia);
+            LiczbaTrans++;
+        }
+
+        static public void UsuńTransakcję(Transakcja transakcjia)
+        {
+            SpisTrans.Remove(transakcjia);
+            LiczbaTrans--;
+        }
+
+        static public void UsuńWszystkie()
+        {
+            SpisTrans.Clear();
+            LiczbaTrans = 0;
+        }
+
+        static public bool JestTransakcją(Transakcja transakcjia)
+        {
+            foreach (Transakcja t in SpisTrans)
+                if (t.Equals(transakcjia)) return true;
+            return false;
+        }
+
+    }
+}
